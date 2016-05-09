d08060c2897e9459746331a161bab22ca196c5e1
yichengjie <626659321@qq.com>
2016-05-09 10:35:56 +0800

    time在入口的时候引入了

--- ocmoni/src/script/edit/controllers/index.js
+++ ocmoni/src/script/edit/controllers/index.js
@@ -1,7 +1,6 @@
 //主要用来加载各个控制器（所有的控制器都将在这个文件中被加载）,除此之外再不用做其他，
 //因为我们可以有很多个控制器文件，按照具体需要进行添加。
  //需要的插件
- require("tui-timepicker") ;
  require('./eidtController') ;
  //头部
  require("./headController") ;

2aeb37695179f1a2569d698f057c756631b91fad
yichengjie <626659321@qq.com>
2016-05-27 16:20:53 +0800

    新界面整理

---
+++ ocmoni3/html/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/5/27.
+ */
+$(function(){
+    $("#moreQuerySection").hide() ;
+    $("#showHideMoreQuerySectionBtn").click(function(){
+        $("#moreQuerySection").slideToggle() ;
+    }) ;
+}) ;

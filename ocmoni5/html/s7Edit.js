0c1ae7695b1816edda120e26e18c12f19178ba9f
yichengjie <626659321@qq.com>
2016-06-01 10:44:41 +0800

    edit页面样式重新规划

---
+++ ocmoni5/html/s7Edit.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/5/30.
+ */
+$(function(){
+    $('.datepicker').datepicker({
+        language:'zh-CN',
+        format:'yyyy/mm/dd',
+        startDate:new Date()
+    });
+}) ;

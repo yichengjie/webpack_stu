fb466c20d142c217fd50bf7a8e55d5b3d883f66c
yichengjie <626659321@qq.com>
2016-06-04 17:28:32 +0800

    commit

--- ocmoni5/js/s7Edit.js
+++ ocmoni5/js/s7Edit.js
@@ -9,4 +9,22 @@ $(function(){
         orientation: "bottom",
         autoclose:true
     });
+
+
+    $('#autocomplete').autocomplete({
+        minChars:0,
+        lookup: function (query, done) {
+            var result = {
+                suggestions: [
+                    { "value": "10011", "data": "10011" },
+                    { "value": "10012", "data": "10012" },
+                    { "value": "10013",  "data": "10013" }
+                ]
+            };
+            done(result);
+        },
+        onSelect: function (suggestion) {
+            alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+        }
+    });
 }) ;

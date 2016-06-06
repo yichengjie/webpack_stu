542d07c059b81047b0569c06452f7d60f48e77f4
yichengjie <626659321@qq.com>
2016-06-06 16:53:47 +0800

    drop menu加入

--- ocmoni6/js/s7Query.js
+++ ocmoni6/js/s7Query.js
@@ -7,4 +7,15 @@ $(function(){
         $("#moreQuerySection").slideToggle() ;
     }) ;
     $('.datepicker').datepicker({minDate:new Date(), showButtonPanel:true});
+
+    $(".dropdown-oc").find(".dropdown-trigger").click(function (e) {
+        e.stopPropagation() ;
+        $(".dropdown-menu-oc").removeClass('open') ;
+        $(this).parents(".dropdown-oc").find(".dropdown-menu-oc").toggleClass('open') ;
+    }) ;
+
+    $(document).click(function(e){
+        e.stopPropagation() ;
+        $('.dropdown-menu-oc').removeClass('open') ;
+    }) ;
 }) ;

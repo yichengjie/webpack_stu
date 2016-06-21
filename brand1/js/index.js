1a4dd31e712513853c2d1ceda72d48ef35c204b0
yichengjie <626659321@qq.com>
2016-06-21 16:22:47 +0800

    brand查询页面整理

--- brand1/js/index.js
+++ brand1/js/index.js
@@ -3,5 +3,22 @@
  */
 $(function(){
     //初始化headroom插件
-    $('#myheader').headroom();
+    //$('#myheader').headroom();
+    //.collapse('toggle')
+    $(document).delegate('.brand_title>.left','click',function(){
+        //$(this).parents('.panel').find('.panel-body').collapse('toggle');
+        //$(this).parents('.panel').find('.panel-body').toggle(300) ;
+        $(this).parents('.panel').find('.panel-body').toggleClass('hidden') ;
+        var isHidden = $(this).parents('.panel').find('.panel-body').hasClass('hidden') ;
+        var showHideBtn = $(this).find('[data-name=showHideBody]');
+        showHideBtn.removeClass('glyphicon-download') ;
+        showHideBtn.removeClass('glyphicon-upload') ;
+        if(isHidden){
+            showHideBtn.addClass('glyphicon-download') ;
+        }else{
+            showHideBtn.addClass('glyphicon-upload') ;
+        }
+        console.info(flag) ;
+    }) ;
+
 }) ;
